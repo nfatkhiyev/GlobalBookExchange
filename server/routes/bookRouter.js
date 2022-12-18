d4f2974c34c8bd78cc9ffbba84d3c73fe09ef9d6
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2022-12-18 15:30:46 -0600

    add book progress

--- server/routes/bookRouter.js
+++ server/routes/bookRouter.js
@@ -8,33 +8,72 @@ router.get('/', (req, res) => {
 });
 
 router.get('/add', authMiddleware, (req, res) => {
-    res.render('books/add', { layout: 'layouts/formPageLayout', error: '', });
+    res.render('books/add', { layout: 'layouts/formPageLayout', errorISBN: '', errorTitle: '', });
 });
 
 router.post('/add', authMiddleware, (req, res) => {
     res.send('Book Added');
 });
 
-router.get('/find-by-isbn', authMiddleware, (req, res) => {
-    const url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + req.query['isbn'];
+router.get('/add-by-isbn', authMiddleware, (req, res) => {
+    const url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + req.query['addBookISBN'] + '&printType=books';
     fetch(url, {
         method: 'GET',
     })
         .then(resp => resp.json())
         .then(data => {
-            if(data.totalItems > 0){
-                const bookInfo = data.items[0].volumeInfo;
-                const params = {
-                    title: bookInfo.title,
-                    author: bookInfo.author,
-                    publisher: bookInfo.publisher,
-                    publishedDate: bookInfo.publishedDate,
-                    description: bookInfo.description,
-                    thumbnailLink: bookInfo.imageLinks.thumbnail,
+            if (data.totalItems > 0) {
+                bookListData = {
+                    totalBooks: data.totalItems,
+                    bookData: [],
                 };
-                res.json(params);
+                for (let book in data.items) {
+                    const bookInfo = book.volumeInfo;
+                    const params = {
+                        title: bookInfo.title,
+                        author: bookInfo.authors,
+                        publisher: bookInfo.publisher,
+                        publishedDate: bookInfo.publishedDate,
+                        description: bookInfo.description,
+                        thumbnailLink: bookInfo.imageLinks.thumbnail,
+                    };
+                    bookListData.bookData.push(params);
+                }
+                res.render('books/add-by-search', { layout: 'layouts/interiorPageLayout', bookList: bookListData });
             } else {
-                res.render('books/add', { layout: 'layouts/fromPageLayout', error: 'No Books Found.'});
+                res.render('books/add', { layout: 'layouts/formPageLayout', errorISBN: 'No Books Found', errorTitle: '', });
+            }
+        })
+        .catch(error => console.log(error));
+});
+
+router.get('/add-by-title', authMiddleware, (req, res) => {
+    const url = 'https://www.googleapis.com/books/v1/volumes?q=intitle:"' + req.query['addBookTitle'] + '"inauthor:"' + req.query['addBookAuthor'] + '"&printType=books';
+    fetch(url, {
+        method: 'GET',
+    })
+        .then(resp => resp.json())
+        .then(data => {
+            if (data.totalItems > 0) {
+                bookListData = {
+                    totalBooks: data.totalItems,
+                    bookData: [],
+                };
+                for (let bookIndex in data.items) {
+                    const bookInfo = data.items[bookIndex].volumeInfo;
+                    const params = {
+                        title: bookInfo.title,
+                        authors: bookInfo.authors,
+                        publisher: bookInfo.publisher,
+                        publishedDate: bookInfo.publishedDate,
+                        description: bookInfo.description,
+                        thumbnailLink: (bookInfo.imageLinks?bookInfo.imageLinks.thumbnail:''),
+                    };
+                    bookListData.bookData.push(params);
+                }
+                res.render('books/add-by-search', { layout: 'layouts/interiorPageLayout', bookList: bookListData, });
+            } else {
+                res.render('books/add', { layout: 'layouts/formPageLayout', errorISBN: '', errorTitle: 'No Books Found', });
             }
         })
         .catch(error => console.log(error));
